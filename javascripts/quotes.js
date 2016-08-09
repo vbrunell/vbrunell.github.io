b6629941c51ff482dbf9f5c2fc1828aa1aedb816
Denshizuno <buruneru@gmail.com>
2016-08-09 17:38:16 -0400

    foobar

--- javascripts/quotes.js
+++ javascripts/quotes.js
@@ -14,7 +14,7 @@
 	Quotation[11] = "To err is human, but to really foul things up you need a computer. - Paul Ehrlich -";
 	Quotation[12] = "Any code of your own that you haven’t looked at for six or more months might as well have been written by someone else. - Eagleson’s Law -";
 	Quotation[13] = "Any problem in computer science can be solved with another level of indirection. - David Wheeler -";
-	Quotation[14] = "Talk is cheap. Show me the code. - Linus Torvalds";
+	Quotation[14] = "Talk is cheap. Show me the code. - Linus Torvalds -";
 	Quotation[15] = "I do not fear computers. I fear the lack of them. - Isaac Asimov -";
 	Quotation[16] = "If you don’t know anything about computers, just remember that they are machines that do exactly what you tell them but often surprise you in the result. — Richard Dawkins";
 	Quotation[17] = "The inside of a computer is as dumb as hell but it goes like mad! - Richard P. Feynman -";
